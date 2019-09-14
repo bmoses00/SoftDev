3fb03944d9ae33d98b154fd1947144c2b7d0d073
bmoses00 <34358568+bmoses00@users.noreply.github.com>
2019-09-14 12:10:13 -0400

    Create csv_parser.py

---
+++ 03_csv/csv_parser.py
@@ -0,0 +1,24 @@
+import csv
+import random
+
+OCCUPATIONS = {}
+
+with open('occupations.csv') as csv_file:  #open CSV file
+    csv_reader = csv.reader(csv_file, delimiter=',')  #instantiate CSV reader object
+    line_count = 0  #make sure header isn't included in dictionary
+    for row in csv_reader:  #populate dictionary with keys and values
+        if(line_count == 0):
+            line_count += 1
+        else:
+            OCCUPATIONS[row[0]] = float(row[1]);
+
+def selector():  #function to randomly select an occupation
+    randomValue = random.random() * 99.8
+    threshold = 0
+    for x, y in OCCUPATIONS.items():
+        if(randomValue < (threshold + y)):
+            return(x, y)
+        else:
+            threshold += y
+
+print(selector())
