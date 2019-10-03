b21e53504493e4b4180d5bce9aead56977ecf52b
bmoses00 <bmoses00@stuy.edu>
2019-10-03 09:25:52 -0400

    worked

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
 app = Flask(__name__)
+login = False
 
 @app.route('/')
 def main():
-    session['username'] = 'portenders'
-    session['password'] = 'chicken'
+    print(login)
+    session['username'] = 'chicken'
+    session['password'] = 'portenders'
     print("------------------------------------")
     print(session['username'])
     print("------------------------------------")
     print(session['password'])
+
+    # if (login):
+    #     return render_template("welcome.html", username = request.args.get('Username'))
+
     return render_template("index.html",
                            username = request.args.get('Username'),
                            password = request.args.get('Password'))
 
 @app.route('/auth')
 def authenticate():
-    print(request.args.get('Username'))
-    print(session['username'])
-    if (request.args.get('Username')) == session['username']:
-        return "nice"
+    if ((request.args.get('Username')) == session['username']
+    and request.args.get('Password') == session['password']):
+        login = True
+        print(login)
+        return render_template("welcome.html", username = request.args.get('Username'))
     else:
-        return "not nice"
+        return render_template("error.html"
+                             , username = request.args.get('Username')
+                             , password = request.args.get('Password'))
     #return redirect("http://www.xkcd.com")
     # return render_template("response.html")
-    return 'what'
+    return 'this did not work'
 
 if __name__ == "__main__":
         app.secret_key = os.urandom(32)
