76be9793ef47c1a9d814e9f758d5bb463f334fbe
Brian <bmoses00@stuy.edu>
2019-10-09 14:48:25 -0400

    Finished 17

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -6,33 +6,50 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
-with open("students.csv", "r") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row)
-        print(row["name"])
-
-DB_FILE="discobandit.db"
+DB_FILE = "discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
+# first table
 
-# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+# beginning and ending of each c.execute command
+beginning = "INSERT INTO students VALUES ("
+ending = ");"
 
-# test SQL stmt in sqlite3 shell, save as string
-
-command = "CREATE TABLE students (name TEXT PRIMARY KEY, id INTEGER);"
+command = "CREATE TABLE students (name TEXT PRIMARY KEY, age INTEGER, id INTEGER);"
 c.execute(command)
-command = "INSERT INTO students VALUES ('Bob', 2531);"
+
+with open("students.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        # add each row of the ordered dict to the db table
+        command = beginning + "'" + row["name"] + "', " + row["age"] + ", " + row["id"] + ending
+        c.execute(command)
+
+# this code prints the table
+# command = "SELECT * FROM students;"
+# c.execute(command)
+# print(c.fetchall())
+
+#==========================================================
+# second table
+command =  "CREATE TABLE courses (code, mark INTEGER, id INTEGER);"
 c.execute(command)
-command = "SELECT * FROM students;"
-c.execute(command)
-print(c.fetchall())
 
+beginning = "INSERT INTO courses VALUES ("
+ending = ");"
 
+with open("courses.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        command = beginning + "'" + row["code"] + "', " + row["mark"] + ", " + row["id"] + ending
+        c.execute(command)
 
+# command = "SELECT * FROM courses;"
+# c.execute(command)
+# print(c.fetchall())
 
 #==========================================================
 
