38eec4fddd8557be267d41d0bbfbeb88ef715f1e
bmoses00 <bmoses00@stuy.edu>
2019-11-18 16:53:55 -0500

    Made no progress

--- test_dir/app.py
+++ test_dir/app.py
@@ -35,7 +35,25 @@ def hello_world():
     u = urllib.request.urlopen(link)
     response = u.read()
     weather = json.loads(response)
+    
+
+  #https://github.com/Yelp/yelp-fusion/blob/master/fusion/php/sample.php
+    
+    api_key='tKiZ_qS57cBSfmAxfCXF0sHinmPEG4v0pvANltq9hDLPUIDmM5zDdWbjy89BB5feUMcLFZtT_uVu5RHOnzfHGBUnZoPeVAbvmxemTEyk_WLA0B6FXnbnB59GfuDQXXYx'
+    
+    headers = {'Authorization': 'Bearer %s' % api_key}
+	
+    link = "https://api.yelp.com/v3/businesses/search"
+    
+    params = {'term':'seafood','location':'New York City'}
+    
+    req=requests.get(url, params=params, headers=headers)
+    
+    yelp_data = json.loads(req.text)
+    
     return "Weather in your area: " + weather["consolidated_weather"][0]["weather_state_name"]
+
+    
 
 if __name__ == "__main__":
     app.debug = True
