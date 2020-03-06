210c1e258066215d38a6cb7d2082b76729fad5ba
Brian <bmoses00@stuy.edu>
2020-03-06 09:26:38 -0500

    Did work on assignment

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -5,7 +5,11 @@
 
 from pymongo import MongoClient
 from flask import Flask, render_template, request, session, redirect, url_for
+
 app = Flask(__name__)
+client = MongoClient() # sets to localhost automatically
+db = client.MosDB
+col = db.movies
 
 @app.route('/')
 def landing():
@@ -13,11 +17,28 @@ def landing():
 
 @app.route('/gross_results')
 def gross():
-    client = MongoClient() # sets to localhost automatically
-    db = client.MosDB
-    col = db.movies
-    print (request.args['gross'])
-    return render_template("index.html", gross = [movie_data["Title"] for movie_data in col.find({"US_Gross": {"$gt": gross}})]) #add results
+    lis = list(col.find({"US_Gross": {"$gt": int(request.args['gross'])}}))
+    return render_template("index.html", gross = lis) #add results
+
+@app.route('/ratings_results')
+def ratings():
+    lis = list(
+                col.find({
+                    "Rotten_Tomatoes_Rating": {"$gt": int(request.args['rotten_tomatoes'])},
+                    "IMDB_Rating": {"$gt": int(request.args['imdb'])}
+                })
+            )
+    return render_template("index.html", ratings = lis)
+
+@app.route('/year_results')
+def year():
+    lis = [movie for movie in col.find({}) if int(movie["Release_Date"][-4:]) > int(request.args['year'])]
+    return render_template("index.html", year = lis)
+
+@app.route('/genre_results')
+def genre():
+    lis = [movie for movie in col.find({"Major_Genre": request.args['genre']})]
+    return render_template("index.html", genre = lis)
 
 if __name__ == "__main__":
         app.debug = True
