6d579aa12417aacec3a6117f37778b1ccfc6d29a
Brian <bmoses00@stuy.edu>
2020-03-30 09:37:59 -0400

    Completed assignment

---
+++ 13_svg/svg.js
@@ -0,0 +1,35 @@
+var pic = document.getElementById("vimage");
+var btn = document.getElementById("clear");
+
+let c;
+
+var clear = function() {
+  pic.innerHTML = '';
+}
+
+var plot = function(e) {
+  c = document.createElementNS(
+  "http://www.w3.org/2000/svg", "circle");
+  c.setAttribute("cx", e.offsetX);
+  c.setAttribute("cy", e.offsetY);
+  c.setAttribute("r", 20);
+  c.setAttribute("fill", "blue");
+  c.addEventListener("click", circle_clicked);
+
+  pic.appendChild(c);
+}
+
+var circle_clicked = function(e) {
+  if (e.originalTarget.getAttribute("fill") == "blue") {
+    e.originalTarget.setAttribute("fill", "cyan");
+  }
+  else {
+    e.originalTarget.setAttribute("cx", Math.random() * pic.getAttribute("width"));
+    e.originalTarget.setAttribute("cy", Math.random() * pic.getAttribute("width"));
+    e.originalTarget.setAttribute("fill", "blue");
+  }
+  e.stopPropagation();
+}
+
+btn.addEventListener("click", clear);
+pic.addEventListener("click", plot);
