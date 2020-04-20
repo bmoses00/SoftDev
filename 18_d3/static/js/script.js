b457e7e1a2ca2ff1a6198fad6246ec8f7188abe6
Brian <bmoses00@stuy.edu>
2020-04-20 18:04:05 -0400

    Completed assignment

--- 18_d3/static/js/script.js
+++ 18_d3/static/js/script.js
@@ -1,93 +1,188 @@
-var margin = {top: 100, left: 100};
+// declare margins and dimensions
+var margin = {bottom: 100, left: 100, top: 50};
 var width = 1000 - margin.left;
-var height = 500 - margin.top;
+var height = 500 - margin.bottom - margin.top;
 
-var curtain_offset = 200;
+var svg_margin = 10;
 
+// global variables
+var c = d3.select(".curtain");
+var svg = d3.select("#graph");
+var txt = document.getElementById("text");
 
-var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left)
-  .attr("height", height + margin.top)
-  .append("g")
-  .attr("transform",
-      "translate(" + margin.left + ",0)");
+var x;
+var y;
+var valueline;
 
-
-// set the ranges
-var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-/*
-function x(input) {
-  return (width - 0) * input;
-}
-
-function y(input) {
-  return (0 - height) * input;
-}
-*/
-
-// define the line
-var valueline = d3.line()
-
-    .x(function(d) { return x(d.Year); })
-    .y(function(d) { return y(d.co2); });
-
-
-// Get the data
-d3.csv("static/csv/afghanistan.csv").then(function(data) {
-
-  // format the data
-  data.forEach(function(d) {
-      d.Year = +d.Year;
-      d.co2 = +d.co2;
-  });
-
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.Year; }));
-  y.domain([0, d3.max(data, function(d) { return d.co2; })]);
-
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", valueline);
-
-  // Add the x Axis
-  svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
-
-  // Add the y Axis
-  svg.append("g")
-  .call(d3.axisLeft(y));
-});
-
-
-var c = d3.select(".curtain")
-  .style("width", width)
-  .style("height", height)
-  .style("position", "absolute")
-  .style("top", 0)
-  .style("left", curtain_offset)
-  .style("background-color", "white")
+var count = 0;
 
 var animation_id;
 
-var render_graph = function() {
-  let left = +c.style("left").slice(0, c.style("left").length - 2);
-  let width = +c.style("width").slice(0, c.style("width").length - 2);
+function draw_graph(country, dataset, color) {
 
-  let change_factor = 8;
+  d3.csv("static/csv/" + dataset).then(function(data) {
 
-  c.style("left", left + change_factor);
-  c.style("width", width - change_factor);
+    data.forEach(function(d) {
+          d.Year = +d.Year;
+          d[country] = +d[country];
+    });
 
-  if (left < width + margin.left + curtain_offset)
-    animation_id = window.requestAnimationFrame(render_graph);
+    create_path(country, dataset, color, data);
+    create_labels(country, dataset, data);
+
+    reveal_curtain();
+  });
+}
+
+function create_path(country, dataset, color, data) {
+  // create the bounding box for the graph
+  svg = d3.select("#graph")
+    .attr("width", width + margin.left)
+    .attr("height", height + margin.bottom + margin.top)
+    .append("g")
+    .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+  // create the functions relating the data to x, y coordinates of points
+  x = d3.scaleLinear()
+    .range([0, width])
+    .domain([1990, 2013]);
+  y = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(data, function(d) { return d[country]; })]);
+
+  valueline = d3.line()
+    .x(function(d) { return x(d.Year); })
+    .y(function(d) { return y(d[country]); });
+
+    // draw line
+    svg.append("path")
+        .attr("class", "path")
+        .data([data])
+        .attr("stroke", color)
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+        .attr("d", valueline);
+}
+
+function create_labels(country, dataset, data) {
+
+  let x_label_container = document.getElementById("x_label");
+  let y_label_container = document.getElementById("y_label");
+  let axis_bottom_container = document.getElementById("axis_bottom")
+  let axis_left_container = document.getElementById("axis_left")
+  let title_container = document.getElementById("title");
+
+  let y_label;
+
+  if (dataset == "co2_per_gdp.csv")         y_label = "co2 emissions (kg) per $ of GDP (PPP)"
+  else if (dataset == "co2_per_capita.csv") y_label = "co2 emissions (metric tons) per capita"
+  else                                      y_label = "co2 emissions (kilotons)"
+
+  let title = country + " - " + y_label;
+
+  // if the function has already been called once
+  if (x_label_container != null) {
+    // remove previous lines
+    var paths = document.getElementsByClassName("path");
+    for (let i = 0; i < paths.length - 1; i++) {
+      paths[i].remove();
+    }
+    // update y-axis and title
+    y_label_container.innerHTML = y_label;
+    title_container.innerHTML = title;
+
+    // destroy and re-create x and y axes
+    axis_bottom_container.remove();
+    svg.append("g")
+      .attr("id", "axis_bottom")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    axis_left_container.remove();
+    svg.append("g")
+      .attr("id", "axis_left")
+      .call(d3.axisLeft(y));
+  }
+
+  else {
+    // create x and y axis labels
+    svg.append("g")
+      .attr("id", "axis_bottom")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    svg.append("g")
+      .attr("id", "axis_left")
+      .call(d3.axisLeft(y));
+
+    // create x and y axes
+    svg.append("text")
+      .attr("id", "x_label")
+      .attr("transform",
+            "translate(" + (width/2) + " ," + (height + 50) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
+    svg.append("text")
+    .attr("id", "y_label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text(y_label);
+
+    // create title
+    svg.append("text")
+      .attr("id", "title")
+      .attr("x", width / 2)
+      .attr("y", -margin.top / 2)
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .style("text-decoration", "underline")
+      .text(title);
+  }
+
+  // put the curtain in place
+  c
+    .style("width", width)
+    .style("height", height + 2)
+    .style("position", "absolute")
+    .style("top", margin.top + svg_margin - 4)
+    .style("left", margin.left + svg_margin)
+    .style("background-color", "white");
+
+  // console.log(c.style("left").slice(0, c.style("left").length - 2));
+}
+
+function render_graph() {
+  if (count == 0) {
+    draw_graph("United States", "co2_per_gdp.csv", "blue");
+    txt.innerHTML = "<br><br><center><b>US co2 emissions per $ of GDP have been in decline over the past 30 years</b></center>";
+  }
+  if (count == 1) {
+    draw_graph("United States", "co2.csv", "red");
+    txt.innerHTML = "<br><br><center><b>However, due to economic growth, co2 emissions overall have remained roughly constant</b></center>";
+    btn.remove();
+  }
+  count++;
+};
+
+function reveal_curtain() {
+
+  let curtain_left = +c.style("left").slice(0, c.style("left").length - 2);
+  // first time reveal_curtain is called, c.style("left") equals 'auto'
+  if (isNaN(curtain_left)) curtain_left = margin.left + svg_margin;
+  let curtain_width = +c.style("width").slice(0, c.style("width").length - 2);
+
+  let change_factor = 10;
+
+  c.style("left", curtain_left + change_factor);
+  c.style("width", curtain_width - change_factor);
+  if (curtain_left < width + margin.left)
+    animation_id = window.requestAnimationFrame(reveal_curtain);
   else {
     window.cancelAnimationFrame(animation_id);
   }
 }
-var btn = document.getElementById("render");
+
+var btn = document.getElementById("render")
 btn.addEventListener("click", render_graph);
